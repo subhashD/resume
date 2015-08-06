62f0d68220706ba6102d516465522df1d502de03
Alec Fenichel <fenichelar@gatech.edu>
2015-08-06 17:05:07 -0400

    Add Google Analytics

--- js/resume.js
+++ js/resume.js
@@ -47,6 +47,17 @@ $(document).ready(function() {
 
         // Set page title
         document.title = data.name + " - Resume";
+
+        // Setup Google Analytics
+        if (data.analytics) {
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', data.analytics, 'auto');
+            ga('send', 'pageview');
+        }
     })
 
     // Output error status on failure to load JSON
